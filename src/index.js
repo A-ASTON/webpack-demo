933c73eeb5368cf409f87d9f125e2136a6719886
A-ASTON <LiangZiFeng666@outlook.com>
2019-08-10 12:01:55 +0800

    'first_commit'

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import './style.css';
+import Img from './img.png';
+
+function component() {
+    const element = document.createElement('div');
+  
+    //Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    //Add the image to our existing div.
+    const myImg = new Image();
+    myImg.src = Img;
+
+    element.appendChild(myImg);
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [    //define rules
+            {
+                test: /\.css$/,             //any file ends with '.css'   
+                use: [                      //will be served to the style-loader and the css-loader 
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
