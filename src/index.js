bdd6a3d1c7e36eb994c69497588980cf3ef6ac6a
A-ASTON <LiangZiFeng666@outlook.com>
2019-08-12 15:37:02 +0800

    '2019/08/12'

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import _ from 'lodash';
-import './style.css';
-import Img from './img.png';
+// import './style.css';
+// import Img from './img.png';
+// import Data from './data.xml';
+import printMe from './print';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     //Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack', 'success'], ' ');
+    
+    // //Loading CSS
+    // element.classList.add('hello');
 
-    //Add the image to our existing div.
-    const myImg = new Image();
-    myImg.src = Img;
-
-    element.appendChild(myImg);
+    // //Add the image to our existing div.
+    // const myImg = new Image();
+    // myImg.src = Img;
+    // element.appendChild(myImg);
+    
+    // //Loading data
+    // console.log(Data);
   
+    //ouput management
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,72 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    mode: 'development', //set mode to 'development'
+
+    // entry: './src/index.js',
+    //rewrite the entry like this:
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'   //it will generate the files called ~/dist/app.bundle.js and ~/dist/print.bundle.js
     },
-    module: {
-        rules: [    //define rules
-            {
-                test: /\.css$/,             //any file ends with '.css'   
-                use: [                      //will be served to the style-loader and the css-loader 
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
+
+    //
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+
+    //manage the plugins
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            titleL: 'Output Management'
+        })
+    ],
+
+    //manage the output files
+    output: {
+        filename: '[name].bundle.js',  //there is a change here too 
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
+
+    
+    // module: {
+    //     rules: [    //define rules
+    //         {
+    //             test: /\.css$/,             //any file ends with '.css'   
+    //             use: [                      //will be served to the style-loader and the css-loader 
+    //                 'style-loader',
+    //                 'css-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(png|svg|jpg|gif)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(csv|tsv)$/,
+    //             use: [
+    //                 'csv-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.xml$/,
+    //             use: [
+    //                 'xml-loader'
+    //             ]
+    //         }
+    //     ]
+    // }
 };
